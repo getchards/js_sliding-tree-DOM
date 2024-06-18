adaba2b769b324c422892fab972ec125965a1b3e
getchards <kazansasha02@gmail.com>
2024-06-18 20:45:54 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,33 @@
+"use strict";
+const slidingLists = document.querySelectorAll("span");
+slidingLists.forEach((span)=>{
+    span.addEventListener("click", ()=>{
+        const parentLi = span.parentElement;
+        const childUl = parentLi.querySelector("ul");
+        if (childUl) toggleVisibility(childUl);
+    });
+});
+const toggleVisibility = (element)=>{
+    if (element.style.display !== "none") element.style.display = "none";
+    else element.style.display = "";
+};
+function createSpansForCategories() {
+    const tree = document.querySelector(".tree");
+    const listItemsWithUl = tree.querySelectorAll("li > ul");
+    listItemsWithUl.forEach((ul)=>{
+        const parentText = ul.parentElement.firstChild.textContent;
+        ul.parentElement.firstChild.textContent = "";
+        if (!ul.parentElement.querySelector("span.sliding-span")) {
+            const span = document.createElement("span");
+            span.textContent = parentText;
+            span.classList.add("sliding-span");
+            span.addEventListener("click", ()=>{
+                toggleVisibility(ul);
+            });
+            ul.parentElement.insertBefore(span, ul);
+        }
+    });
+}
+createSpansForCategories();
+
+//# sourceMappingURL=index.f75de5e1.js.map
